ac08aabd0a5797549fbcfd41dbc173a88284e4dc
Lenard Pratt <striderman34@gmail.com>
2016-11-11 21:52:34 +0000

    Refactor js
    restart and use vanilla js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,15 @@
-var workBook = [];
+var taskList = [];
 
-var globalIndex = 1;
-function addNewWork() {
-  task = {};
-  task.title = document.getElementById('titleInput').value;
-  task.description = document.getElementById('descriptionInput').value;
-  task.index = globalIndex++;
-  task.date = document.getElementById('datepicker').value;
-  task.done = false;
-  workBook.push(task);
+
+function Task(text) {
+    this.text = text;
+    this.date = new Date();
+    this.done = false;
 }
 
-/*
-addNewWork();
-addNewWork();
-addNewWork();
+var taskInput = document.getElementById("taskInput");
 
-*/
-var bin;
-function deleteTask(tableRow) {
-
-  workBook = workBook.splice(tableRow, 1);
-}
-
-function reloadTable() {
-  $('tbody').empty();
-  for (var i = 0; i < workBook.length; i++) {
-    var tr;
-    tr = $('<tr id =' + workBook[i].index + '>');
-    tr.append('<td>' + workBook[i].index + '</td>');
-    tr.append('<td>' + workBook[i].title + '</td>');
-    tr.append('<td>' + workBook[i].description + '</td>');
-    tr.append('<td>' + workBook[i].date + '</td>');
-tr.append(" <td> <div class='form-check'" + "id=taskNum" + workBook[i].index + ">" + "<button type='button' class='btn btn-danger'" + "id=taskRemove" + workBook[i].index + ">Finished Task</button></div> </td>");
-    $('tbody').append(tr);
-  };
-
-};
+var list = document.getElementById("list");
 
 
-$(document).ready(function() {
-  $('#saveChanges').click(function() {
-
-    addNewWork();
-    reloadTable();
-
-
-
-    // For deleting a Task will move  when Ajax implemented
-    $('td').children().click(function() {
-
-      var tableRowId = this.id;
-      var match = tableRowId.replace(/\D/g,'');
-      console.log(tableRowId);
-      console.log(workBook);
-
-
-      $('#'+tableRowId).click(function() {
-        $('.alert').show().delay(800).fadeOut(400);
-          reloadTable();
-          deleteTask(match);
-        });
-    });
-  });
-});
+list.addEventListener
