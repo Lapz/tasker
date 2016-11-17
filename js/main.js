99769da1cacd67bf8d2f1e5f3a653d069924a5cf
Lenard Pratt <striderman34@gmail.com>
2016-11-17 19:42:37 +0000

    Started commenting and documenting my code
    Refer to the title

--- js/main.js
+++ js/main.js
@@ -11,26 +11,38 @@ var Task = function(text) {
 
 var Elements = function() {
 
+    // creates the necessary element
     this.row = document.createElement("div"); // creates the boostrap row
-
-    this.row.className = "row"; // assigns the classs row
 
     this.col8 = document.createElement("div"); // creates the col to hold the content 8
 
-    this.col8.className = "col-xs-6"; // assigns the classs for the col
-
-
-
-    this.col2Date = document.createElement("div"); // creates the col to hold the the date
-
-    this.col2Date.className = "col-xs-4"; // assigns the classs for the col for the date
+    this.col4Date = document.createElement("div"); // creates the col to hold the the date
 
     this.col2Input = document.createElement("div"); // creates the col to hold the tick box
 
+    this.input = document.createElement("input"); // creates an input element
+
+    this.li = document.createElement("li") // creates the li element to nest to the div
+
+    this.textP = document.createElement("p"); // creates a text element to hold the task text itself
+
+    this.dateP = document.createElement("p"); // creates a text element to hold the date from the task's date
+
+    // Sets the className
+
+    this.row.className = "row"; // assigns the classs row
+
+    this.col8.className = "col-xs-6"; // assigns the classs for the col
+
+    this.col4Date.className = "col-xs-4"; // assigns the classs for the col for the date
+
     this.col2Input.className = "col-xs-2"; // assigns the classs for the col that holds the input
 
+    this.row.className = "itemWrapper"; // sets the class for the row
 
-    this.input = document.createElement("input"); // creates an input element
+    this.textP.className = "info" // sets the class of the textP element to other element
+
+    // Sets up the input button
 
     this.input.type = "checkbox"; // sets the inputs type to checkbox
 
@@ -38,107 +50,70 @@ var Elements = function() {
 
     this.col2Input.appendChild(this.input); // inserts the element into the div that will hold it
 
-
-
-    this.li = document.createElement("li")
-
-    this.row.className = "itemWrapper";
-
-    this.textP = document.createElement("p");
-
-    this.textP.className = "info"
-
-    this.dateP = document.createElement("p");
-
-
-    // creates a list element <script src="js/vendor/jquery-3.1.1.min.js"></script>
+    // sets up the structure
 
     /*
-<li>  <script src="js/vendor/bootstrap.js"></script>
-    <div class="row">
-        <div class="col-xs-6">
 
-        </div>
+    <li>
+     <div class= "row">
 
-        <div class="col-xs-4">
+       <div class = "col-xs-8">
+         </div>
+
+       <div class = "col-xs-4">
+          </div>
+
+        <div class = "col-xs-2">
+          </div>
         </div>
-        <div class="col-xs-2">
-            <input type="checkbox" />
-        </div>
-    </div>
-</li>
+    </li>
+
     */
 
-    this.row.appendChild(this.col8);
+    this.row.appendChild(this.col8); // appends the div with a class of col8 into the row
 
-    this.row.appendChild(this.col2Date);
+    this.row.appendChild(this.col4Date); // appends the div with a class of col4 into the row
 
-    this.row.appendChild(this.col2Input);
+    this.row.appendChild(this.col2Input); // appends the div woth a class of col2 into the row
 
-    this.li.appendChild(this.row); // inserts the row into li
-
+    this.li.appendChild(this.row); // inserts the row into li element
 
 }
 
 
-var addbtn = document.getElementById("addTask");
-
-var ul = document.getElementById("list")
 
 
-function createTask(tasktext) {
-    // Need to refractor and change the css as welll as the do speed benchmark test. Also create a new branch. Add a new class fot the p elemetns and the css appploie s to evert single one of the elemetens
+
+function createTask(tasktext, list) {
 
     var task = new Task(tasktext); // create the task
 
     var el = new Elements(); // creates the Elements
 
-    var newContent = document.createTextNode(task.text);
+    var taskText = document.createTextNode(task.text); // a var to hold the text for the task
 
-    el.textP.appendChild(newContent);
+    var date = task.date.getTime() + " on the" + task.date.getDate() + "/" + task.date.getMonth() + "/" + task.date.getFullYear(); // creaets a string that will hold the time that task was created
 
-    el.dateP.appendChild(document.createTextNode(task.date.getDate() + "/" + task.date.getMonth() + "/" + task.date.getFullYear()));;
+    el.textP.appendChild(taskText); // appends the taskText to
 
-    //console.log(el.li.childNodes[0].childNodes)
+    el.dateP.appendChild(document.createTextNode(date));
 
     el.li.childNodes[0].childNodes[0].appendChild(el.textP);
 
     el.li.childNodes[0].childNodes[1].appendChild(el.dateP);
 
-    console.log(ul.childNodes.length);
-
-    //console.log(el.li);
-
-
-    ul.appendChild(el.li);
-
-
-    //console.log(el.li.childNodes[0].childNodes)
-
-
-
-    /*
-        el.extraDiv.appendChild(date);
-
-        el.li.appendChild(newContent);
-
-        el.li.appendChild(el.extraDiv);
-
-        el.div.appendChild(el.li);
-        list.appendChild(el.div);
-        */
+    list.appendChild(el.li);
 
 }
 
-function addTask() {
+function addTask(list) {
 
     var taskInput = document.getElementById("taskInput").value;
-
 
     if (taskInput == false) {
         return null;
     } else {
-        createTask(taskInput);
+        createTask(taskInput, list);
     }
 
 }
@@ -164,45 +139,15 @@ function deleteTask() {
 
 }
 
-/*
-    var arr = [];
-
-    ul.childNodes.forEach(function(e) {
-
-        if (e.nodeType == Node.ELEMENT_NODE)
-            arr.push(e);
-
-        //console.log(e.childNodes);
-
-
-    })
-
-    //console.log(arr);
-
-    for (var i = 0; i < arr.length; i++) {
-        console.log(arr[i].childNodes.values());
-    }
-*/
-//console.log(e);*/
-
-//console.log(ul.children)
-
-/*
-    el.childNodes.forEach(function(e) {
-        if (e.childNodes[1].childNodes[0].checked == true) {
-            e.parentNode.parentNode.removeChild(e.parentNode)
-        }
-
-    })*/
-
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    var addbtn = document.getElementById("addTask");
+
+    var ul = document.getElementById("list");
+
     addbtn.addEventListener("click", function() {
-        if (addTask() === null) {
+        if (addTask(ul) === null) {
             console.log("fuck");
         };
     });
